ef5eb6dd27fd7bc8a80d4d1c66df2ff62058e6c5
Sanyalak T. <isanyalakt@gmail.com>
2025-05-28 11:03:25 +0700

    update(feat): urlsearchparams and get submissions to last topics as optimisticUI

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -1,17 +1,35 @@
-import { Form, Link, NavLink, Outlet, useNavigation, } from "react-router";
+import { Form, Link, NavLink, Outlet, useNavigation, useSubmit, } from "react-router";
 import { getContacts } from "../data";
 import type { Route } from "./+types/sidebar";
+import { useEffect } from "react";
 
-export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
+export async function loader({
+  request
+}: Route.LoaderArgs) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
 }
 
 export default function SidebarLayout({
   loaderData,
 }: Route.ComponentProps) {
-  const { contacts } = loaderData;
+  const { contacts, q } = loaderData;
   const navigation = useNavigation();
+  const submit = useSubmit();
+  const searching =
+  navigation.location &&
+  new URLSearchParams(navigation.location.search).has(
+    "q"
+  );
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
 
   return (
     <>
@@ -20,9 +38,18 @@ export default function SidebarLayout({
           <Link to="about">React Router Contacts</Link>
         </h1>
         <div>
-          <Form id="search-form" role="search">
+          <Form id="search-form"
+            onChange={(event) => {
+              const isFirstSearch = q === null;
+              submit(event.currentTarget, {
+                replace: !isFirstSearch,
+              });
+            }}
+            role="search">
             <input
               aria-label="Search contacts"
+              className={searching ? "loading" : ""}
+              defaultValue={q || ""}
               id="q"
               name="q"
               placeholder="Search"
@@ -30,7 +57,7 @@ export default function SidebarLayout({
             />
             <div
               aria-hidden
-              hidden={true}
+              hidden={!searching}
               id="search-spinner"
             />
           </Form>
@@ -74,7 +101,7 @@ export default function SidebarLayout({
       </div>
 
       <div className={
-          navigation.state === "loading" ? "loading" : ""
+          navigation.state === "loading" && !searching ? "loading" : ""
         }
       id="detail">
         <Outlet />
--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,9 +1,19 @@
-import { Form } from "react-router";
+import { Form, useFetcher } from "react-router";
 
 import type { ContactRecord } from "../data";
 
-import { getContact } from "../data";
+import { getContact, updateContact } from "../data";
 import type { Route } from "./+types/contact";
+
+export async function action({
+  params,
+  request,
+}: Route.ActionArgs) {
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+}
 
 export async function loader({ params }: Route.LoaderArgs) {
   const contact = await getContact(params.contactId);
@@ -82,21 +92,26 @@ function Favorite({
 }: {
   contact: Pick<ContactRecord, "favorite">;
 }) {
-  const favorite = contact.favorite;
+  const fetcher = useFetcher();
+  const favorite = fetcher.formData
+    ? fetcher.formData.get("favorite") === "true"
+    : contact.favorite;
 
   return (
     <Form method="post">
-      <button
-        aria-label={
-          favorite
-            ? "Remove from favorites"
-            : "Add to favorites"
-        }
-        name="favorite"
-        value={favorite ? "false" : "true"}
-      >
-        {favorite ? "★" : "☆"}
-      </button>
+      <fetcher.Form method="post">
+        <button
+          aria-label={
+            favorite
+              ? "Remove from favorites"
+              : "Add to favorites"
+          }
+          name="favorite"
+          value={favorite ? "false" : "true"}
+        >
+          {favorite ? "★" : "☆"}
+        </button>
+      </fetcher.Form>
     </Form>
   );
 }
